fd1f2cf18e1b878646fe59fd953006475122a2b9
DevinNorgarb <devin.norgarb@pargo.co.za>
2024-03-02 01:55:19 +0200

    init

---
+++ app/Events/LocationUpdated.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ *
+ */
+class LocationUpdated implements ShouldBroadcast
+{
+    use Dispatchable,InteractsWithSockets,SerializesModels;
+
+    public $location;
+
+    public function __construct(Location $location)
+    {
+        $this->location = $location;
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('location.' . $this->location->user_id);
+    }
+
+}
+
